f025bcf62f822a57acfd298f4a51aedecf6088c3
Carlo Lunetta <c.lunetta@reply.it>
2022-02-26 18:28:21 +0100

    change render city info

--- src/Components/Common/CityInfo/CityInfo.js
+++ src/Components/Common/CityInfo/CityInfo.js
@@ -2,18 +2,12 @@ import { useState } from 'react';
 import './CityInfo.scss';
 
 export default function CityInfo() {
-    
 
     const render = () => {
         return (
             <div className='city-info'>
-                <div className='city-name'>
-                    <p>San Francisco</p>
-                </div>
-
-                <div className='state-name'>
-                    <p>California</p>
-                </div>
+                <p className='city-name'>San Fransisco</p>
+                <p className='state-name'>CALIFORNIA</p>
             </div>
         );
     }
